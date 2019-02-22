5a86b2259eddc86f548865d188c216ccad4f3c51
meherett <meherett@zoho.com>
2019-02-22 23:45:52 +0300

    init unittest CobraTest class added

---
+++ test/__init__.py
@@ -0,0 +1,15 @@
+from eth_tester.exceptions import TransactionFailed
+from test.contract import CobraFactory
+from test.account import CobraAccount
+from eth_tester import EthereumTester
+from json import loads
+from web3 import Web3
+import unittest
+
+
+class CobraTest(unittest.TestCase):
+
+
+
+
+
