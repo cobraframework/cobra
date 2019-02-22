7dfd675b3ae9e24fa5cae83771712eb20296f8e8
meherett <meherett@zoho.com>
2019-02-22 23:46:50 +0300

    CobraTester class added on test

--- test/__init__.py
+++ test/__init__.py
@@ -32,4 +32,48 @@ class CobraTest(unittest.TestCase):
         return suite
 
 
+class CobraTester:
+
+    def __init__(self, _web3: Web3, _ethereum_tester: EthereumTester,
+                 compiled_interfaces=None):
+        if compiled_interfaces is None:
+            compiled_interfaces = dict()
+        self.ethereum_tester = _ethereum_tester
+        self.web3 = _web3
+
+        self.compiled_interfaces = compiled_interfaces
+
+    def contract(self, name):
+        for compiled_interface in self.compiled_interfaces.keys():
+            contract_name = compiled_interface.split(":")
+            if contract_name[0] == name:
+                interface = self.compiled_interfaces.get(compiled_interface)
+                return self.new(interface)
+            else:
+                continue
+
+    def new(self, interface):
+        if isinstance(interface['abi'], str):
+            interface['abi'] = loads(interface['abi'])
+        return CobraFactory(self.web3, interface)
+
+    @property
+    def accounts(self):
+        return [CobraAccount(self.web3, a) for a in self.ethereum_tester.get_accounts()]
+
+    @property
+    def eth(self):
+        # Return the w3 eth API
+        return self.web3.eth
+
+    @property
+    def tx_fails(self):
+        return CobraFailureHandler(self.ethereum_tester)
+
+    def now(self):
+        #  Get this from the Ethereum block timestamp
+        return self.web3.eth.getBlock('pending')['timestamp']
+
+    def mine_blocks(self, number=1):
+        self.ethereum_tester.mine_blocks(number)
 
