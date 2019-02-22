bea26f28ce0794e4a3f7c2800ab76ed113d6dffb
meherett <meherett@zoho.com>
2019-02-22 23:46:23 +0300

    update test

--- test/__init__.py
+++ test/__init__.py
@@ -9,7 +9,27 @@ import unittest
 
 class CobraTest(unittest.TestCase):
 
+    def __init__(self, methodName='runTest', collectionInterfaces=None):
+        super(CobraTest, self).__init__(methodName)
+        if collectionInterfaces is None:
+            collectionInterfaces = dict()
+        try:
+            self.cobra = CobraTester(
+                collectionInterfaces._web3,
+                collectionInterfaces._ethereum_tester,
+                collectionInterfaces.compiled_interfaces)
+        except AttributeError:
+            self.cobra = None
 
+    @staticmethod
+    def cobra(testCase_Class, collectionInterfaces=None):
+        testLoader = unittest.TestLoader()
+        testNames = testLoader.getTestCaseNames(testCase_Class)
+        suite = unittest.TestSuite()
+        for testName in testNames:
+            suite.addTest(testCase_Class(testName, collectionInterfaces=collectionInterfaces))
+
+        return suite
 
 
 
