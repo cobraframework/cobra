79538d42f6862fe369710d833876e5fe38600263
meherett <meherett@zoho.com>
2019-04-11 12:03:09 +0300

    CobraAccount update

--- test/__init__.py
+++ test/__init__.py
@@ -10,17 +10,17 @@ class CobraAccount(str):
 
     def __new__(cls, web3: Web3, address):
         obj = super().__new__(cls, address)
-        obj._web3 = web3
-        obj._address = address
+        obj.web3 = web3
+        obj.address = address
         return obj
 
     # Send Ether
     def transfer(self, address, amount):
-        self._web3.eth.sendTransaction({'to': address, 'from': self._address, 'value': amount})
+        self.web3.eth.sendTransaction({'to': address, 'from': self.address, 'value': amount})
 
     @property
     def balance(self):
-        return self._web3.eth.getBalance(self._address)
+        return self.web3.eth.getBalance(self.address)
 
 
 class CobraTest(unittest.TestCase):
