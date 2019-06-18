19712ca1ccec0120169dfa0c5d32ee97b91c9537
meherett <meherett@zoho.com>
2019-06-18 15:28:56 +0300

    package checker added on utils

--- cobra/utils/__init__.py
+++ cobra/utils/__init__.py
@@ -1,56 +1,26 @@
 import json
 import sys
 import yaml
+import pkg_resources
 
-from .console_log import console_log
+from cobra.utils.package_checker import package_checker
+from cobra.utils.console_log import console_log
+from cobra.utils.utils import (
+    file_reader,
+    yaml_loader,
+    json_loader,
+    strip
+)
 
-
-def file_reader(file_path):
-    try:
-        with open(file_path, 'r') as read_file:
-            return_file = read_file.read()
-            read_file.close()
-            return return_file
-    except FileNotFoundError:
-        console_log("No such file or directory '%s'" % file_path,
-                    "error", "FileNotFoundError")
-    sys.exit()
-
-
-def yaml_loader(yaml_file, more=False):
-    try:
-        load_compile = yaml.load(yaml_file)
-        return load_compile
-    except yaml.scanner.ScannerError as scannerError:
-        if more:
-            console_log(str(scannerError),
-                        "error", "ScannerError")
-        else:
-            console_log(str(scannerError).split('\n')[0],
-                        "error", "ScannerError")
-    except yaml.parser.ParserError as parserError:
-        if more:
-            console_log(str(parserError),
-                        "error", "ParserError")
-        else:
-            console_log(str(parserError).split('\n')[0],
-                        "error", "ParserError")
-    sys.exit()
-
-
-def strip(_strip):
-    return _strip.strip()[1:-1]
-
-
-def json_loader(json_file, more=False):
-    try:
-        loaded_json = json.loads(json_file)
-        return loaded_json
-    except json.decoder.JSONDecodeError as jsonDecodeError:
-        if more:
-            console_log(str(jsonDecodeError),
-                        "error", "JSONDecodeError")
-        else:
-            console_log(str(jsonDecodeError).split('\n')[0],
-                        "error", "JSONDecodeError")
-    sys.exit()
+__all__ = [
+    "sys",
+    "strip",
+    "json",
+    "pkg_resources",
+    "json_loader",
+    "yaml_loader",
+    "package_checker",
+    "file_reader",
+    "yaml",
+    "console_log",
+]
