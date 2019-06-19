d951943a4bfa3d1a7d50867d9d17db2d58c9f8c6
meherett <meherett@zoho.com>
2019-06-19 22:50:46 +0300

    network added on cli

---
+++ cobra/cli/network.py
@@ -0,0 +1,14 @@
+from cobra import *
+
+
+def main(more=False):
+    configuration = Configuration()
+    read_yaml = file_reader("./cobra.yaml")
+    load_yaml = yaml_loader(read_yaml, more=more)
+    if 'network' in load_yaml:
+        network_yaml = load_yaml['network']
+        configuration_yaml = configuration.network(network_yaml)
+        return configuration_yaml
+    else:
+        console_log("network in cobra.yaml", "error", "NotFound")
+        sys.exit()
