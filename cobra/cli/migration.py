65d5b5fc7042fc84bfe190427db77987d2f06f13
meherett <meherett@zoho.com>
2019-06-20 05:45:33 +0300

    migration.py added on cli

---
+++ cobra/cli/migration.py
@@ -0,0 +1,36 @@
+from cobra.cli.network\
+    import main as main_network
+from cobra import *
+
+
+def main(more=False):
+    network = main_network(more)
+    deployment = Deployment(network, more)
+    configuration = Configuration()
+    read_yaml = file_reader("./cobra.yaml")
+    load_yaml = yaml_loader(read_yaml, more=more)
+    if 'deploy' in load_yaml:
+        deploy_yaml = load_yaml['deploy']
+        configurations_yaml = configuration.deploy(deploy_yaml)
+        # deployment.display_account()
+        for configuration_yaml in configurations_yaml:
+            if configuration_yaml['links'] is None:
+                artifact_path_json = join(configuration_yaml['artifact_path_dir'], configuration_yaml['artifact'])
+                artifact_json = deployment.deploy_with_out_link(
+                    configuration_yaml['artifact_path_dir'],
+                    configuration_yaml['artifact'], more=more)
+                if artifact_json is not None:
+                    file_writer(artifact_path_json, str(artifact_json))
+                continue
+            else:
+                artifact_path_json = join(configuration_yaml['artifact_path_dir'], configuration_yaml['artifact'])
+                artifact_json = deployment.deploy_with_link(
+                    configuration_yaml['artifact_path_dir'],
+                    configuration_yaml['artifact'],
+                    configuration_yaml['links'], more=more)
+                if artifact_json is not None:
+                    file_writer(artifact_path_json, str(artifact_json))
+                continue
+    else:
+        console_log("deploy in cobra.yaml", "error", "NotFound")
+        sys.exit()
