6536d852e11e733edfa4d89c90292eeb07612e9e
meherett <meherett@zoho.com>
2019-06-22 00:18:44 +0300

    removed example

--- examples/Mercy.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.0;
-
-library Mercy {
-    function mercy(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
-	{
-		return amount * conversionRate;
-	}
-}
